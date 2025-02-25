d24e607401b3f21b1ba4c415626c4500b0144729
MuhammadRaffey <muhammadraffey26@gmail.com>
2025-02-25 13:18:59 +0500

    changed background

--- src/app/sections/Hero.tsx
+++ src/app/sections/Hero.tsx
@@ -1,6 +1,6 @@
 import { inter } from "../fonts/inter";
 import { motion } from "framer-motion";
-import HeroBackground from "../../components/background/HeroBackground";
+import { WavyBackground } from "@/components/ui/wavy-background";
 import React from "react";
 import AnimatedTitle from "../animations/AnimatedTitle";
 
@@ -12,21 +12,22 @@ const Hero = () => {
       initial="initial"
       animate="animate"
     >
-      <HeroBackground />
-      <div className="mt-10 flex flex-col items-center justify-center sm:mt-0">
-        <div
-          className={`relative flex flex-col items-center justify-center ${inter.className} pointer-events-none`}
-        >
-          <AnimatedTitle
-            text={"Hi   I'm   Muhammad Raffey."}
-            className={
-              "mb-1 text-left text-[40px] font-bold leading-[0.9em] tracking-tighter text-[#e4ded7] sm:text-[45px] md:mb-16 md:text-[60px] lg:text-[80px] ml-4"
-            }
-            wordSpace={"mr-[10px]"}
-            charSpace={"mr-[0.001em]"}
-          />
+      <WavyBackground>
+        <div className="mt-10 flex flex-col items-center justify-center sm:mt-0">
+          <div
+            className={`relative flex flex-col items-center justify-center ${inter.className} pointer-events-none`}
+          >
+            <AnimatedTitle
+              text={"Hi   I'm   Muhammad Raffey."}
+              className={
+                "mb-1 text-left text-[40px] font-bold leading-[0.9em] tracking-tighter text-[#e4ded7] sm:text-[45px] md:mb-16 md:text-[60px] lg:text-[80px] ml-4"
+              }
+              wordSpace={"mr-[10px]"}
+              charSpace={"mr-[0.001em]"}
+            />
+          </div>
         </div>
-      </div>
+      </WavyBackground>
     </motion.section>
   );
 };
